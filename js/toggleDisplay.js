cf44e82b309ffce5fa018ea4702a2974b8c3eb91
rycchen <159330273+rycchen@users.noreply.github.com>
2024-02-26 08:21:36 -0500

    Add files.

---
+++ js/toggleDisplay.js
@@ -0,0 +1,15 @@
+function toggleDisplay(abst) {
+
+  if(document.getElementById(abst).style.display == "none" ) {
+
+  document.getElementById(abst).style.display = "";
+
+  }
+
+  else {
+
+  document.getElementById(abst).style.display = "none";
+
+  }
+
+}
